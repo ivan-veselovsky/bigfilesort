62efff33cd0ca3dd3fb0a1aac65a37c8d2af4e25
Ivan Veselovsky <iveselovsky@griddynamics.com>
2013-07-22 20:20:59 +0400

    test-write-sort-verify.sh: provided usage printout, + sorting parameters passing.

--- test-write-sort-verify.sh
+++ test-write-sort-verify.sh
@@ -1,4 +1,17 @@
-#!/bin/bash -ex
+#!/bin/bash -e
+
+if [ "$#" -lt 4 ]; then
+  echo "Generates data file with random data, sorts it, then verifies the sorting."
+  echo "Parameters: [-c] [-m] <file_name> <file_size[k,m,g,t]> <number_of_threads> <max_native_memory_alloc_Mb>"
+  echo "-c, -m options are to be passed to 'bigfilesort' script."
+  exit 1
+fi
+
+BIGFILESORT_OPTS=""
+while [ "$#" -gt 4 ]; do
+  BIGFILESORT_OPTS="${BIGFILESORT_OPTS} ${1}"
+  shift
+done
 
 FILE=$1
 SIZE=$2
@@ -7,7 +20,7 @@ ALLOC_MB=$4
 
 ./write-data-file.sh "${FILE}" "${SIZE}" rand
 
-./bigfilesort.sh  "${FILE}" "${THREADS}" "${ALLOC_MB}"
+./bigfilesort.sh ${BIGFILESORT_OPTS} "${FILE}" "${THREADS}" "${ALLOC_MB}"
 
 ./check-sorting.sh "${FILE}" 
 
